b0650a134dbb7e4ad2487c21a23f361d5c98db15
Tom Flídr <tomflidr@gmail.com>
2021-06-09 14:42:24 +0200

    release v5.0.17

--- src/MvcCore/Ext/Models/Db/Model.php
+++ src/MvcCore/Ext/Models/Db/Model.php
@@ -31,6 +31,6 @@ implements	\MvcCore\IModel,
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.16';
+	const VERSION = '5.0.17';
 
 }
