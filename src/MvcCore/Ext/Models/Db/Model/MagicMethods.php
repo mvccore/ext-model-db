2cd95eb97a98a1427d99f4654b6a280d03897ae1
Tom Flídr <tomflidr@gmail.com>
2023-01-27 22:31:54 +0100

    Removed extended `__clone()` method, leaving base `__clone()` method.

--- src/MvcCore/Ext/Models/Db/Model/MagicMethods.php
+++ src/MvcCore/Ext/Models/Db/Model/MagicMethods.php
@@ -19,39 +19,6 @@ namespace MvcCore\Ext\Models\Db\Model;
 trait MagicMethods {
 
 	/**
-	 * @return void
-	 */
-	public function __clone () {
-		list ($metaData, $sourceCodeNamesMap) = static::GetMetaData(
-			static::$defaultPropsFlags, [\MvcCore\Ext\Models\Db\Model\IConstants::METADATA_BY_CODE]
-		);
-		$phpWithTypes = PHP_VERSION_ID >= 70400;
-		foreach ($sourceCodeNamesMap as $propertyName => $metaDataIndex) {
-			list ($propIsPrivate) = $metaData[$metaDataIndex];
-			$currentValue = NULL;
-			if ($propIsPrivate) {
-				$prop = new \ReflectionProperty($this, $propertyName);
-				$prop->setAccessible(TRUE);
-				if ($phpWithTypes)
-					if (!$prop->isInitialized($this))
-						continue;
-				$currentValue = $prop->getValue($this);
-			} else if (isset($this->{$propertyName})) {
-				$currentValue = $this->{$propertyName};
-			}
-			if (!is_object($currentValue)) continue;
-			$clonedValue = clone $currentValue;
-			if ($propIsPrivate) {
-				$prop->setValue($this, $clonedValue);
-			} else {
-				$this->{$propertyName} = $clonedValue;
-			}
-			if (isset($this->initialValues[$propertyName]))
-				$this->initialValues[$propertyName] = $clonedValue;
-		}
-	}
-
-	/**
 	 * @inheritDocs
 	 * @param  int $propsFlags
 	 * @return mixed
@@ -59,7 +26,7 @@ trait MagicMethods {
 	#[\ReturnTypeWillChange]
 	public function jsonSerialize ($propsFlags = 0) {
 		if ($propsFlags === 0) $propsFlags = static::$defaultPropsFlags;
-		$data = static::GetValues(static::$defaultPropsFlags, TRUE);
+		$data = static::GetValues($propsFlags, TRUE);
 		return array_filter($data, function ($val) {
 			return !is_resource($val) && !($val instanceof \Closure);
 		});
